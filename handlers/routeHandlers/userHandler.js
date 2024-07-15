d4c3004576539abb7a3a02288604b8d07c65cfa8
MD Azmain  Ausaf Abesh <118210671+AvoidAzmain@users.noreply.github.com>
2024-07-15 21:33:57 +0600

    userHandler created successfully

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -1,6 +1,7 @@
 // dependencies
 const data = require ('../../lib/data');
-const{hash} = require('./../../helpers/utilities') ;
+const{hash} = require('../../helpers/utilities') ;
+const{parseJson} = require('../../helpers/utilities')
 // module sccafolding
 const handler = {};
 
@@ -86,10 +87,125 @@ handler._users.post = (requestProperties, callback) => {
       }
 };
 
-handler._users.get = (requestProperties, callback) => {};
+handler._users.get = (requestProperties, callback) => {
+    // check the phone number is valid or not
+    const phone =
+    typeof requestProperties.queryStringObject.phone === "string" &&
+    requestProperties.queryStringObject.phone.trim().length === 11
+      ? requestProperties.queryStringObject.phone
+      : false;
 
-handler._users.put = (requestProperties, callback) => {};
+      if(phone){
+        // lookup the user 
+        data.read('users',phone,(err,u) =>{
+            if(!err && u){
+                const user ={...parseJson(u)};
+                // remove the hashed password from the user object before returning it to the client
+                delete user.password;
+                callback(200, user);
+            }
+            else {
+                callback(404,{
+                    error:'phone number is not valid 222',
+                })
+            }
+        })
+      }else{
+        callback(404,{
+            error:'phone number is not valid',
+        })
+      }
+};
 
-handler._users.delete = (requestProperties, callback) => {};
+handler._users.put = (requestProperties, callback) => {
+    const firstName =
+    typeof requestProperties.body.firstName === "string" &&
+    requestProperties.body.firstName.trim().length > 0
+      ? requestProperties.body.firstName
+      : false;
+
+  const lastName =
+    typeof requestProperties.body.lastName === "string" &&
+    requestProperties.body.lastName.trim().length > 0
+      ? requestProperties.body.lastName
+      : false;
+
+  const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone
+      : false;
+
+  const password =
+    typeof requestProperties.body.password === "string" &&
+    requestProperties.body.password.trim().length > 0
+      ? requestProperties.body.password
+      : false;
+
+
+      if(phone){
+        if(firstName|| lastName || password){
+            data.read('users',phone,(err,uData)=>{
+                const userData = {...parseJson(uData)};
+                if(!err && userData){
+                    if(firstName){
+                        userData.firstName = firstName;
+                    }
+
+                    if(lastName){
+                        userData.lastName = lastName;
+                    }
+
+                    if(password){
+                        userData.password = hash(password);
+                    }
+
+                    //store to the file
+                    data.update('users',phone,userData,(err2)=>{
+                        if(!err2){
+                            callback(200,{error: 'user updated'});
+                        }else{
+                            callback(500,{error: 'server side issue'});
+                        }
+                    })
+                }else{
+                    callback(404,{error: 'you have problem in your input'});
+                }
+            })
+        }else{
+            callback(404,{error: 'check your data again'});
+        }
+      }else{
+        callback(404,{error: 'invalid phone number'});
+      }
+
+};
+
+handler._users.delete = (requestProperties, callback) => {
+    const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone
+      : false;
+
+      if(phone){
+        data.read('users',phone,(err,userData)=>{
+            if(!err && data){
+                data.delete('users',phone,(err2)=>{
+                    if(!err2){
+                        callback(200,{error: 'user deleted'});
+                    }else{
+                        callback(500,{error: 'server side issue'});
+                    }
+                })
+            }
+            else{
+                callback(500,{error: 'server side error'});
+            }
+        })
+      }else {
+        callback(404,{error: 'invalid phone number'});
+      }
+};
 
 module.exports = handler;
